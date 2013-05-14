682d0e77c5cc4cef69b5f50e1df450a03a3fec22
Lance Pollard <lancejpollard@gmail.com>
2013-05-13 21:44:53 -0700

    create todos

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var model = require('tower-model')
   , query = require('tower-query')
   , router = require('tower-router')
   , route = require('tower-route')
-  , scope = require('tower-scope')
-  , template = require('tower-template');
+  , scopes = require('tower-scope')
+  , template = require('tower-template')
+  , memory = require('tower-memory-adapter')
+  , directive = require('tower-directive');
 
 /**
  * Models.
@@ -36,18 +38,48 @@ route('/:filter')
  * Scopes.
  */
 
-scope('body')
+scopes('body')
+  .attr('todos', 'array', [])
   .action('newTodo', newTodo)
   .action('clearCompleted', clearCompleted)
   .action('toggleCompleted', toggleCompleted)
   .action('removeTodo', removeTodo);
 
 /**
+ * Custom directives.
+ */
+
+directive('data-each', function(scope, element, attr){
+  var self = this;
+  var val = attr.value.split(/ +/);
+  element.removeAttribute('data-each');
+
+  if (val.length > 1) {
+    var name = val[0];
+    var prop = val[2];
+  } else {
+    var prop = val[0];
+  }
+
+  // e.g. todos
+  var array = scope.get(prop);
+  var templateFn = template(element);
+  var parent = element.parentNode;
+  parent.removeChild(element);
+
+  scope.on('change ' + prop, function(){
+    var childScope = scopes('todo').init();
+    var childElement = templateFn.clone(childScope);
+    parent.appendChild(childElement);
+  });
+});
+
+/**
  * Templates.
  */
 
 var fn = template('body', document.body);
-fn(scope.root());
+fn(scopes.root());
 
 /**
  * Create a new `todo`.
@@ -57,8 +89,9 @@ function newTodo(event) {
   if (!enterKey(event)) return;
   var title = $(event.target).val();
   // callback b/c adapters can be async (AJAX, sockets, etc.)
+
   model('todo').create({ title: title }, function(err, todo){
-    scope('body').emit('change todos');
+    scopes('body').changed('todos');
   });
 }
 
